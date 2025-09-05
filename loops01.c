2e0e542202d68561f88f8dd8578a74656b41a26e
Amaresh Kumar <amareshkumar7525@gmail.com>
2025-09-05 07:40:48 +0530

    Add files via upload

---
+++ loops01.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+int main()
+{
+    // for(int i = 1; i<=5; i++){
+    //     printf("%d\n", i);
+    // }
+    int a = 1;
+    while (a <= 7)
+
+    {
+        printf("%d\n", a);
+        a++; /* code */
+    }
+
+    return 0;
+}
