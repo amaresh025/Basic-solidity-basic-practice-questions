53a753d41f99edd1936aa824c59176d97cd04099
Amaresh Kumar <amareshkumar7525@gmail.com>
2025-09-09 10:00:38 +0530

    Added : Retrieve student by id

---
+++ Structs/04_Retrive_By_Id.sol
@@ -0,0 +1,33 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.21;
+
+contract RetriveById {
+    // Define a struct "Student"
+    // Each student will have: name, marks, and a unique ID
+    struct Student {
+        string name;
+        uint256 marks;
+        uint256 id; // unique student id
+    }
+
+    // Dynamic array to store multiple students
+    Student[] public students;
+
+    // Function to add a new student into the array
+    // Takes name, marks, and id as input
+    function addStudents(string memory _name, uint256 _marks, uint256 _id) public {
+        students.push(Student(_name, _marks, _id));
+    }
+
+    // Function to retrive student details by their ID
+    // Loops through all students and returns the details if ID matches
+    function retrive(uint256 _id) public view returns (string memory, uint, uint) {
+        for (uint i = 0; i < students.length; i++) {
+            if (students[i].id == _id) {
+                return (students[i].name, students[i].marks, students[i].id);
+            }
+        }
+        // If no student found with given ID, revert the transaction
+        revert("Id didn't found");
+    }
+}
