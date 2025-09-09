ccc3d336430caeb5017fb943f29ae2689a5433b8
Amaresh Kumar <amareshkumar7525@gmail.com>
2025-09-09 08:46:43 +0530

    Update 01_Student_Struct.sol

--- Structs/01_Student_Struct.sol
+++ Structs/01_Student_Struct.sol
@@ -11,27 +11,6 @@ contract SimpleStruct {
         uint id;
     }
 
-    // Dynamic array of Student structs
-    Student[] public students;
-
-    // Function to add a new student into the array
-    function addStudent(string memory _name, uint _marks, uint _id) public {
-        // Push a new Student struct into the students array
-        students.push(Student(_name, _marks, _id));
-    }
-
-    // Function to get student details by ID
-    // Returns name, marks, and id if found, otherwise reverts
-    function getStudent(uint _id) public view returns (string memory, uint, uint) {
-        // Loop through all students in the array
-        for (uint i = 0; i < students.length; i++) {
-            // If the student's id matches the given _id
-            if (students[i].id == _id) {
-                // Return the student's details
-                return (students[i].name, students[i].marks, students[i].id);
-            }
-        }
-        // If no student with the given id is found, throw an error
-        revert("ID not found");
-    }
+  
 }
+
